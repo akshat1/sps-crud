9f544523bb45edb869adead81c0ca5a81933b2ac
akshat1 <akshat.sh@gmail.com>
2017-07-21 05:53:32 +0000

    - fixed bug. - added more tests.

--- test/index.js
+++ test/index.js
@@ -14,7 +14,15 @@ const FakeLogger = {
   error: noop
 };
 
-const FakeMongoCollection = {};
+const FakeCursor = {};
+FakeCursor.skip = sinon.stub().returns(FakeCursor);
+FakeCursor.limit = sinon.stub().returns(FakeCursor);
+FakeCursor.sort = sinon.stub().returns(FakeCursor);
+FakeCursor.toArray = sinon.stub().returns(Promise.resolve(['foo', 'bar', 'baz']));
+
+const FakeMongoCollection = {
+  find: sinon.stub().returns(FakeCursor)
+};
 // This is what you will get from mongodb.client.connect
 const FakeMongoDatabase = {
   collection: sinon.stub().returns(FakeMongoCollection)
@@ -53,9 +61,43 @@ describe('simian-store', function() {
     assert.equal(1, FakeMongoClient.connect.callCount, 'Should have called connect only once');
   });
 
-  it('_makeVertex');
-  it('_makeEdge');
-  it('get');
+  it('_makeVertex', function() {
+    const src = {
+      foo: 'foo',
+      bar: 'bar'
+    };
+    const expected = Object.assign(src, { _type: Store.Type.Vertex });
+    const actual = Store._makeVertex(src);
+    assert.deepEqual(actual, expected, 'Should be the same as src, expected _type: vertex');
+  });
+
+  it('_makeEdge', function() {
+    const src = {
+      foo: 'foo',
+      bar: 'bar'
+    };
+    const expected = Object.assign(src, { _type: Store.Type.Edge });
+    const actual = Store._makeVertex(src);
+    assert.deepEqual(actual, expected, 'Should be the same as src, expected _type: edge');
+  });
+
+  it('get', async function() {
+    const query = {};
+    const sort = {};
+    const args = {
+      query,
+      skip: 3,
+      limit: 5,
+      sort
+    };
+    const result = await Store.get(args);
+    assert.ok(Array.isArray(result.data), 'result should have property data as []');
+    assert.equal(result.skip, args.skip, 'result should have included skip');
+    assert.equal(result.limit, args.limit, 'result should have included limit');
+    assert.equal(result.sort, args.sort, 'result should have included sort');
+    assert.deepEqual(result.data, ['foo', 'bar', 'baz'], 'result missing expected data');
+  });
+
   it('put');
   it('remove');
   it('relate');
