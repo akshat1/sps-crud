579867bb9e2a0082174578e215e6d46bc996f52d
akshat1 <akshat.sh@gmail.com>
2017-10-12 09:23:18 -0700

    More tests.

--- test/Edge.js
+++ test/Edge.js
@@ -1,18 +1,84 @@
 const db = require('../app/db');
-// const assert = require('assert');
+const assert = require('assert');
 
 const {
-  // Edge,
+  Edge,
+  EdgeType,
+  Vertex,
   sync,
 } = db;
 
 describe('Edge', function() {
+  let fromId, toId;
+
   before(function() {
     return sync({ force: true });
   });
 
-  it('Create');
-  it('Retrieve');
-  it('Update');
-  it('Delete');
+  it('Create', function() {
+    return Promise
+      .all([
+        EdgeType.create({
+          name: 'ET0',
+          metadata: {},
+          isDirectional: true
+        }),
+        Vertex.create({
+          name: 'V0',
+          metadata: {}
+        }),
+        Vertex.create({
+          name: 'V1',
+          metadata: {}
+        })
+      ])
+      .then(function([edgeType, vertex0, vertex1]) {
+        fromId = { vertex0 };
+        toId = { vertex1 };
+        return Edge.create({
+          edgeTypeId: edgeType.id,
+          fromId: vertex0.id,
+          toId: vertex1.id,
+          metadata: { foo: 'bar' },
+          strength: 1
+        });
+      })
+      .then(function(edge) {
+        assert(!!edge, 'Edge should be truthy');
+      });
+  });
+
+  it('Retrieve', function() {
+    return Edge
+      .findOne({
+        fromId,
+        toId
+      })
+      .then(function(edge) {
+        assert(!!edge, 'Did not find edge');
+      });
+  });
+
+  it('Update', function() {
+    return Edge
+      .findOne({
+        fromId,
+        toId
+      })
+      .then(function(edge) {
+        assert(!!edge, 'Did not find edge to update it');
+        return edge.update({ metadata: { baz: 'qux' } });
+      })
+      .then(function(updatedInstance) {
+        assert(updatedInstance.metadata.baz === 'qux', 'name should have been updated');
+      });
+  });
+
+  it('Delete', function() {
+    return Edge
+      .findById(1)
+      .then(function(instance) { return instance.destroy(); })
+      .then(function() { return Edge.findById(1); } )
+      .then(function (instance) { return assert(!instance, 'instance should be falsy')});
+  });
 });
--- test/EdgeType.js
+++ test/EdgeType.js
@@ -1,8 +1,8 @@
-const db = require('../app/db');
-// const assert = require('assert');
+const db     = require('../app/db');
+const assert = require('assert');
 
 const {
-  // EdgeType,
+  EdgeType,
   sync,
 } = db;
 
@@ -11,8 +11,43 @@ describe('EdgeType', function() {
     return sync({ force: true });
   });
 
-  it('Create');
-  it('Retrieve');
-  it('Update');
-  it('Delete');
+  it('Create', function() {
+    return EdgeType
+      .create({
+        name: 'edgetype-0',
+        metadata: {},
+        isDirectional: true
+      })
+      .then(function(instance) {
+        assert(!!instance, 'instance should be truthy');
+      })
+  });
+
+  it('retrieve', function() {
+    // Because we always force sync the db, the edgetype is always 1
+    return EdgeType
+      .findById(1)
+      .then(function(instance) {
+        assert(!!instance, 'instance should be truthy');
+      });
+  });
+
+  it('Update', function() {
+    return EdgeType
+      .findById(1)
+      .then(function(instance) {
+        return instance.update({ name: 'updated-name' });
+      })
+      .then(function(updatedInstance) {
+        assert(updatedInstance.name === 'updated-name', 'name should have been updated');
+      });
+  });
+
+  it('delete', function() {
+    return EdgeType
+      .findById(1)
+      .then(function(instance) {
+        return instance.destroy();
+      });
+  });
 });
